c49470bab32a71c8f6d3e6b07b10158e2e4f9831
Daniel Martínez <danieldt08@gmail.com>
2016-05-12 00:06:13 -0300

    Added API Schema

--- routes/apis.js
+++ routes/apis.js
@@ -4,7 +4,13 @@ var router = express.Router();
 var db = require('mongoose');
 db.connect('mongodb://localhost:27017/test_db');
 
-var API = db.model('apis', {name: String, version: String, jsonString: String});
+var apiSchema = db.Schema({
+  name: String,
+  version: String,
+  jsonString: String
+});
+
+var API = db.model('apis', apiSchema);
 // var newAPI = new API({name: "my_api", version: "1.2", jsonString: '{"user":{"name":{"anyOf":["Daniel","John","Anna"]},"accounts":{"anyOf":[0,1,2,3,4,5,6,7,8,9]}}}'});
 
 /* Helpers */
