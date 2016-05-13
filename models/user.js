e2c5ecbdeffcc4f7e5f818868af967babb7de89b
Daniel Martínez <danieldt08@gmail.com>
2016-05-13 00:19:38 -0300

    Now you can create simple APIs

--- models/user.js
+++ models/user.js
@@ -2,9 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+var API = new Schema({
+  name: String,
+  version: String,
+  jsonString: String
+});
+
 var User = new Schema({
-    username: String,
-    password: String
+  username: String,
+  password: String,
+  apis: [API]
 });
 
 User.plugin(passportLocalMongoose);
--- routes/apis.js
+++ routes/apis.js
@@ -7,6 +7,22 @@ var randomChoice = function(items) {
   return items[Math.floor(Math.random()*items.length)];
 }
 
+router.post('/save', function(req, res) {
+  req.user.apis.push({name: req.body.name, version: req.body.version, jsonString: req.body.jsonString});
+  // req.user.apis.create({name: req.body.name, version: req.body.version, jsonString: req.body.jsonString});
+
+  var subdoc = req.user.apis[0];
+  console.log(subdoc)
+  console.log(subdoc.isNew);
+
+  req.user.save(function (err) {
+    if (err) return handleError(err)
+    console.log('New API saved!');
+  });
+
+  res.redirect('/');
+});
+
 router.get('/:id/:version/:name/', function(req, res, next) {
   API.findOne({"_id": req.params.id, "name": req.params.name, "version": req.params.version}, function(err, api){
     /*if (err) {
